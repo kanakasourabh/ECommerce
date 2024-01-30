b68191572f4b9a49425b103ead794d5420ed226a
Sourabh Kanaka <124447046+kanakasourabh@users.noreply.github.com>
2024-01-30 21:35:36 +0530

    FileToURLPath

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
@@ -13,6 +18,7 @@ dotenv.config();
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 connectDb();
 
@@ -20,8 +26,8 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello from server");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(8000, () => console.log("port is listening on port 8000"));
